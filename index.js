a0db9ff59fd87d08d6964c74a58ea2a3e6264fd4
Shadab Karim <shadab7264karim@gmail.com>
2026-04-09 15:52:56 +0530

    fix mongodb connection

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 /* ================== ✅ MONGODB CONNECT ================== */
-mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/zyro")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log(err));
 
